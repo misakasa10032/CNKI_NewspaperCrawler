d62d0b45b3095f0d5d823e4c69a1f3301f0081c9
misakasa10032 <30925509+misakasa10032@users.noreply.github.com>
2018-10-06 02:38:09 +0800

    Update Generate_TitleList.py

--- Generate_TitleList.py
+++ Generate_TitleList.py
@@ -14,20 +14,19 @@ import pandas as pd
 
 #   The parameters for obtaining the articles
 url_a = 'http://navi.cnki.net/knavi/NPaperDetail/GetArticleDataXsltByDate'
-headers_a = {'Host': 'navi.cnki.net',
+headers_a0 = {'Host': 'navi.cnki.net',
 'Connection': 'keep-alive',
-'Content-Length': '59',
+'Content-Length': '58',
 'Accept': '*/*',
 'Origin': 'http://navi.cnki.net',
 'X-Requested-With': 'XMLHttpRequest',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-'Referer': 'http://navi.cnki.net/KNavi/NPaperDetail?pcode=CCND&bzpym=CZJB'}
-body_a0 = 'py=CZJB&pcode=CCND&pageIndex=1&pageSize=500&date='
+'Referer': 'http://navi.cnki.net/KNavi/NPaperDetail?pcode=CCND&bzpym='}
 
 #   The parameters for obtaining the dates
 url_b = 'http://navi.cnki.net/knavi/NPaperDetail/GetDateGroupList'
-headers_b = {'Host': 'navi.cnki.net',
+headers_b0 = {'Host': 'navi.cnki.net',
 'Connection': 'keep-alive',
 'Content-Length': '28',
 'Accept': '*/*',
@@ -35,23 +34,45 @@ headers_b = {'Host': 'navi.cnki.net',
 'X-Requested-With': 'XMLHttpRequest',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-'Referer': 'http://navi.cnki.net/KNavi/NPaperDetail?pcode=CCND&bzpym=CZJB'}
-body_b0 = 'py=CZJB&pcode=CCND&year='
+'Referer': 'http://navi.cnki.net/KNavi/NPaperDetail?pcode=CCND&bzpym='}
 
 #   Designate the start time and the end time. ATTENTION: The start time and end time must be business day.
-start_time = '2013-01-04'
-end_time = '2018-09-29'
+start_time = '2018-09-26'
+end_time = '2018-09-28'
 
-#   Request for obtaining the source code underlying the dates
-dict_ym = {}
+#   Designate the map from name to code
+n2c_dict = {'中国证券报': 'CZJB', '光明日报': 'GMRB', '经济日报': 'JJRB', '科技日报': 'KJRB', '经济参考报': 'JJCK', '华夏时报': 'HXSB', '证券日报': 'CJRB', '证券时报': 'ZJSB', '上海证券报': 'SHZJ', '中国能源报': 'SHCA', '中国自然资源报': 'GTZY', '中国冶金报': 'CYJB', '中国电力报': 'CDLB', '中国煤炭报': 'CMTB', '中国石油报': 'SHYO', '中国贸易报': 'CMYB', '中国有色金属报': 'YSJS', '中国建材报': 'CJCB', '中国石化报': 'CSHB', '中国黄金报': 'ZGHJ', '中国矿业报': 'CKYB', '中煤地质报': 'ZMDZ', '中国工业报': 'CGYB', '华北电力报': 'HBDL', '地质勘查导报': 'DZKC', '石油管道报': 'SYGD', '铜陵有色报': 'TLYS', '期货日报': 'QHBR'}
+
 def replace_slash(char):
     if '/' in char:
         char = char.replace('/', '-')
     else:
         pass
     return char
+
+def add_zero(m):
+    if m < 10:
+        m = '0' + str(m)
+    else:
+        m = str(m)
+    return m
+
+#   The name of the file must be in English
+out_path = 'E:/outcome.csv'
+
+#   Obtain titles, authors, sources
+df = pd.DataFrame(columns = ['date', 'title', 'author', 'source'], index = ['date'])
+k = 1
+
+#   Designate the name of the newspaper
+news = '期货日报'
+
+#   Request for obtaining the source code underlying the dates
+dict_ym = {}
 for year in range(2000, 2019):
-    body_b = body_b0 + str(year)
+    body_b = 'py=' + n2c_dict[news] + '&pcode=CCND&year=' + str(year)
+    headers_b = headers_b0
+    headers_b['Referer'] = headers_b['Referer'] + n2c_dict[news]
     r_b = requests.post(url = url_b, data = body_b, headers = headers_b)
     String_b = BeautifulSoup(r_b.content.decode(), 'lxml')
     date_list = String_b.find_all(name = 'a', attrs = {'href': 'javascript:void(0);'})
@@ -69,44 +90,49 @@ for year in range(2000, 2019):
 
 #   Ascertain the dates involved.
 date_list = []
-def add_zero(m):
-    if m < 10:
-        m = '0' + str(m)
-    else:
-        m = str(m)
-    return m
-for m_0 in range(int(start_time[5:7]), 13):
-    if m_0 == int(start_time[5:7]):
-        critical_index = dict_ym[start_time[0:7]].index(start_time)
-        date_list.extend(dict_ym[start_time[0:7]][0:critical_index + 1])
-    else:
+if start_time[0:4] != end_time[0:4]:
+    for m_0 in range(int(start_time[5:7]), 13):
+        if m_0 == int(start_time[5:7]):
+            critical_index = dict_ym[start_time[0:7]].index(start_time)
+            date_list.extend(dict_ym[start_time[0:7]][0:critical_index + 1])
+        else:
+            if start_time[0:4] + '-' + add_zero(m_0) in dict_ym.keys():
+                date_list.extend(dict_ym[start_time[0:4] + '-' + add_zero(m_0)][0: ])
+    for y_0 in range(int(start_time[0:4]) + 1, int(end_time[0:4])):
+        for m_0 in range(1,13):
+            if str(y_0) + '-' + add_zero(m_0) in dict_ym.keys():
+                date_list.extend(dict_ym[str(y_0) + '-' + add_zero(m_0)][0: ])
+    for m_0 in range(1, int(end_time[5:7]) + 1):
+        if m_0 == int(end_time[5:7]):
+            critical_index = dict_ym[end_time[0:7]].index(end_time)
+            date_list.extend(dict_ym[end_time[0:7]][critical_index: ])
+        else:
+            if end_time[0:4] + '-' + add_zero(m_0) in dict_ym.keys():
+                date_list.extend(dict_ym[end_time[0:4] + '-' + add_zero(m_0)][0: ])
+elif start_time[0:7] != end_time[0:7]:
+    critical_index = dict_ym[start_time[0:7]].index(start_time)
+    date_list.extend(dict_ym[start_time[0:7]][0:critical_index + 1])
+    for m_0 in range(int(start_time[5:7]) + 1, int(end_time[5:7])):
         date_list.extend(dict_ym[start_time[0:4] + '-' + add_zero(m_0)][0: ])
-for y_0 in range(int(start_time[0:4]) + 1, int(end_time[0:4])):
-    for m_0 in range(1,13):
-        if str(y_0) + '-' + add_zero(m_0) in dict_ym.keys():
-            date_list.extend(dict_ym[str(y_0) + '-' + add_zero(m_0)][0: ])
-for m_0 in range(1, int(end_time[5:7]) + 1):
-    if m_0 == int(end_time[5:7]):
-        critical_index = dict_ym[end_time[0:7]].index(end_time)
-        date_list.extend(dict_ym[end_time[0:7]][critical_index: ])
-    else:
-        date_list.extend(dict_ym[end_time[0:4] + '-' + add_zero(m_0)][0: ])
+    critical_index = dict_ym[end_time[0:7]].index(end_time)
+    date_list.extend(dict_ym[end_time[0:7]][critical_index: ])
+else:
+    critical_index0 = dict_ym[start_time[0:7]].index(start_time)
+    critical_index1 = dict_ym[end_time[0:7]].index(end_time)
+    date_list.extend(dict_ym[start_time[0:7]][critical_index1: critical_index0 + 1])
 
-#   The name of the file must be in English
-out_path = 'E:/outcome.csv'
-
-#   Obtain titles, authors, sources
-df = pd.DataFrame(columns = ['date', 'title', 'author', 'source'], index = ['date'])
-k = 1
+#   Capture the titles
 for date in date_list:
-    body_a = body_a0 + str(date)
+    body_a = 'py=' + n2c_dict[news] + '&pcode=CCND&pageIndex=1&pageSize=500&date=' + str(date)
+    headers_a = headers_a0
+    headers_a['Referer'] = headers_a0['Referer'] + n2c_dict[news]
     r_a = requests.post(url = url_a, data = body_a, headers = headers_a)
     r = r_a.content.decode()
     soup = BeautifulSoup(r, 'lxml')
     for j in soup.find_all(name = 'tr')[1: ]:
         title = j.find(name = 'a', attrs = {'target': '_blank'}).string
         author = j.find_all(name = 'td')[3].string
-        source = '中国证券报'
+        source = news
         df = df.append({'date': pd.to_datetime(date), 'title': title, 'author': author, 'source': source}, ignore_index = True)
     #   Exhibit the proceeding
     print(k/len(date_list))
@@ -115,6 +141,4 @@ for date in date_list:
 #   The output
 df.sort_index()
 df = df.drop([0])
-out_path = 'E:/ZGZQ_part2.csv'
 df.to_csv(out_path)
-            
